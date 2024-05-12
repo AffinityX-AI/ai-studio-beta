fbefc7b1c248761fba2f3bacc4596850ff1378e9
Feliks Gurevich <fgurevich@affinityexpress.com>
2024-05-12 00:32:39 -0400

    Updated code highlighter, remove highlight.io

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
-import { withHighlightConfig } from '@highlight-run/next/config'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
-  experimental: {
-    instrumentationHook: true,
-  },
-  webpack(config, options) {
-    if (options.isServer) {
-      config.ignoreWarnings = [{ module: /highlight-(run\/)?node/ }]
-    }
-
-    return config
-  },
 }
 
-export default withHighlightConfig(nextConfig)
+export default nextConfig
